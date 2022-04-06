ceb8373183d5cf5ebe53475c795c869954eab75f
Amol Ghotale <ghotaleamol@gmail.com>
2022-04-06 19:58:46 +0530

    ADDED: win, loss condition

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,9 +3,17 @@ package com.bridgelabz;
 public class Main {
 
     public static void main(String[] args) {
-	// welcome to gambling problem.
+        // welcome to gambling problem.
 
         int stake = 100;
         int bet = 1;
+
+        int result;
+        result = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println("result: " +result);
+        if (result == 0)
+            System.out.println("Loss");
+        else
+            System.out.println("Win");
     }
 }
