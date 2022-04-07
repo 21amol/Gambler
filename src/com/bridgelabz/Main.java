3cb6633490d0c4c1f33976baccc4a39710909242
Amol Ghotale <ghotaleamol@gmail.com>
2022-04-07 13:28:39 +0530

    ADDED: Resigning condition

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -8,12 +8,30 @@ public class Main {
         int stake = 100;
         int bet = 1;
 
+        int winAmount = 0;
+        int loseAmount = 0;
         int result;
-        result = (int) Math.floor(Math.random() * 10) % 2;
-        System.out.println("result: " +result);
-        if (result == 0)
-            System.out.println("Loss");
-        else
-            System.out.println("Win");
+
+        while (true) {
+            result = (int) Math.floor(Math.random() * 10) % 2;
+            //     System.out.println("result: " + result);
+
+            if (result == 0) {
+                //        System.out.println("Loss");
+                winAmount++;
+
+            } else {
+                //        System.out.println("Win");
+                loseAmount--;
+            }
+
+            if (winAmount == stake / 2) {
+                System.out.println("Win Stake reached 50%. So, Gambler wil resign for the day.");
+                break;
+            } else if (loseAmount == stake / 2) {
+                System.out.println("Lose Stake reached 50%. So, Gambler will resign for the day");
+                break;
+            }
+        }
     }
 }
